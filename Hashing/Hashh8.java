ac0eebf4c98d5046e7498adfb0cc30536dca483c
Nitya Gutti <nityagutti273541@gmail.com>
2026-09-10 20:32:44 +0530

    Add hashing programs 5 to 8

---
+++ Hashing/Hashh8.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Hashh8 {
+    public static void main(String args[]) {
+     Scanner sc = new Scanner(System.in);
+     int n = sc.nextInt();
+     int a[] = new int[n];
+     for (int i = 0; i < n; i++) {
+       a[i] = sc.nextInt();
+     }
+     TreeMap<Integer, Integer> hm = new TreeMap<>(Collections.reverseOrder());
+    for (int i = 0; i < n; i++) {
+          if (hm.containsKey(a[i]))
+              hm.put(a[i], hm.get(a[i]) + 1);
+            else
+              hm.put(a[i], 1);
+     }
+      Set<Integer> s1 = hm.keySet();
+      System.out.println("Element ---- Frequency");
+      for (Integer x : s1) {
+      System.out.println(x + "    " + hm.get(x));
+        }
+    }
+}
