604b054f1334a7afcf96883255420345a6a0a65d
Nitya Gutti <nityagutti273541@gmail.com>
2026-09-12 18:51:24 +0530

    Add hashing programs

---
+++ Hashing/Hashh11.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Hashh11 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        TreeMap<Integer, Integer> hm = new TreeMap<>();
+
+        for (int i = 0; i < n; i++) {
+            hm.put(a[i], hm.getOrDefault(a[i], 0) + 1);
+        }
+        int c = 0;
+        for (int val : hm.values()) {
+            if (val > c) {
+                c = val;
+            }
+        }
+        System.out.println("Elements with max frequency:");
+        for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
+            if (entry.getValue() == c) {
+                System.out.println(entry.getKey());
+            }
+        }
+    }
+}
