0d08879f91c2d35156e78059864fbb083c53db5c
Nitya Gutti <nityagutti273541@gmail.com>
2026-09-17 20:00:32 +0530

    Add hashing programs

---
+++ Hashing/Hashh19.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Hashh19 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) 
+           a[i] = sc.nextInt();
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int x : a) 
+           hm.put(x, hm.getOrDefault(x, 0) + 1);
+        Set<Integer> s1 = hm.keySet();
+        System.out.println("Elements with freq ≥ 2:");
+        for (Integer x : s1) {
+            if (hm.get(x) >= 2) {
+                System.out.println(x);
+            }
+        }
+    }
+}
