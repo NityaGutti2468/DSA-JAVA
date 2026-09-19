99df9da3ff1d429ea2903c6fc3d487f7ad20ca06
Nitya Gutti <nityagutti273541@gmail.com>
2026-09-19 09:51:52 +0530

    Add hashing programs

---
+++ Hashing/Hashh23.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Hashh23 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt(); 
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++)
+         a[i] = sc.nextInt();
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int x : a) 
+           hm.put(x, hm.getOrDefault(x, 0) + 1);
+        Set<Integer> s1 = hm.keySet();
+        System.out.println("Elements with freq ≤ " + k + ":");
+        for (Integer x : s1) {
+            if (hm.get(x) <= k) {
+                System.out.println(x);
+            }
+        }
+    }
+}
