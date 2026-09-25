bb5bef9107779498b53dca56613eaa3cc46a5bbf
Nitya Gutti <nityagutti273541@gmail.com>
2026-09-25 11:10:47 +0530

    Added dynamic programming programs

---
+++ DynamicProgramming/DP9.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class DP9 {
+public static int countPaths(int m,int n) {
+if (m == 1 ||n == 1)
+return 1;
+else 
+return countPaths(m-1,n)+countPaths(m,n-1);
+}
+public static void main(String args[]) {
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter m n values :"); 
+int m = sc.nextInt();
+int n = sc.nextInt();
+System.out.println("Count of Paths : "+countPaths(m,n)); 
+}
+}
